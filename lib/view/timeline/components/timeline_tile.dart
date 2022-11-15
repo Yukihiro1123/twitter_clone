66580ed769c698ca584199d720c8060090d4b51c
Yukihiro1123 <murasima1123@gmail.com>
2022-11-15 14:12:20 +0900

    投稿日時表示

--- lib/view/timeline/components/timeline_tile.dart
+++ lib/view/timeline/components/timeline_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:twitter_clone/style.dart';
 
@@ -32,7 +33,8 @@ class TimelineTile extends StatelessWidget {
                 UserCard(
                   onTap: () => print('Hi'),
                   title: postUser.appUserName,
-                  subTitle: postUser.userId,
+                  subTitle:
+                      DateFormat("yyyy-MM-dd-HH:mm").format(post.postDateTime),
                   trailing: PopupMenuButton(
                     itemBuilder: (BuildContext context) {
                       return [
